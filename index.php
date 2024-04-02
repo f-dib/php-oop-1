4774fb1a1099fcf198fe0e5453b325d49b5f0fe1
federico-dibitonto <f.dibitonto94@gmail.com>
2024-04-02 19:11:34 +0200

    added bonus 1

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
         public $titolo;
         public $durata;
         public $anno_di_uscita;
+        public $genere = [];
         public $vietato_ai_minori;
 
 
@@ -30,17 +31,17 @@
          * @param  mixed $_durata
          * @param  int $_anno_di_uscita
          */
-        function __construct($_titolo, $_durata,  $_anno_di_uscita, Prohibited $_vietato_ai_minori){
+        function __construct($_titolo, $_durata, $_anno_di_uscita, array $_genere, Prohibited $_vietato_ai_minori){
             $this->titolo = $_titolo;
             $this->durata = $_durata;
             $this->anno_di_uscita =  $_anno_di_uscita;
+            $this->genere = $_genere;
             $this->vietato_ai_minori = $_vietato_ai_minori;
         }
     }; 
 
-    $movie1 = new Movie("The Beautiful Game", "2 ore", "2024", new Prohibited(null));
-    var_dump($movie1);
-    $movie2 = new Movie("Povere creature!", "2 ore", "2024", new Prohibited('Vietato ai minori'));
+    $movie1 = new Movie("The Beautiful Game", "2 ore", "2024", ["Drammatico", "Sportivo"], new Prohibited(null));
+    $movie2 = new Movie("Povere creature!", "2 ore", "2024", ["Commedia"], new Prohibited('Vietato ai minori'));
 
     $movies = [
         $movie1,
@@ -66,7 +67,13 @@
 
             echo "
             <li>
-                ". $movie->titolo . ", " . $movie->durata . ", " . $movie->anno_di_uscita . ", " . $movie->vietato_ai_minori?->setProhibited() . "
+                ". $movie->titolo . ", " . $movie->durata . ", " . $movie->anno_di_uscita . ", "; 
+                
+                foreach ($movie->genere as $genere) {
+                    echo $genere . " ";
+                }
+
+                echo $movie->vietato_ai_minori?->setProhibited() . "
             </li>";
 
         }
