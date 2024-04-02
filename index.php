b631314c222d941bd655b335d892aba44ef2fe2d
federico-dibitonto <f.dibitonto94@gmail.com>
2024-04-02 18:27:17 +0200

    milestone completed

--- index.php
+++ index.php
@@ -1,25 +1,46 @@
 <?php
+
+    class Prohibited {
+        public $vietato;
+        
+        /**
+         * __construct
+         *
+         * @param  string $_vietato
+         */
+        function __construct($_vietato){
+            $this->vietato = $_vietato;
+        }
+
+        public function setProhibited() {
+            return $this->vietato;
+        }
+    };
     class Movie {
         public $titolo;
         public $durata;
         public $anno_di_uscita;
+        public $vietato_ai_minori;
+
 
         /**
          * __construct
          *
          * @param  string $_titolo
          * @param  mixed $_durata
-         * @param  int $_durata
+         * @param  int $_anno_di_uscita
          */
-        function __construct($_titolo, $_durata,  $_anno_di_uscita){
+        function __construct($_titolo, $_durata,  $_anno_di_uscita, Prohibited $_vietato_ai_minori){
             $this->titolo = $_titolo;
             $this->durata = $_durata;
             $this->anno_di_uscita =  $_anno_di_uscita;
+            $this->vietato_ai_minori = $_vietato_ai_minori;
         }
     }; 
 
-    $movie1 = new Movie("The Beautiful Game", "2 ore", "2024");
-    $movie2 = new Movie("Povere creature!", "2 ore", "2024");
+    $movie1 = new Movie("The Beautiful Game", "2 ore", "2024", new Prohibited(null));
+    var_dump($movie1);
+    $movie2 = new Movie("Povere creature!", "2 ore", "2024", new Prohibited('Vietato ai minori'));
 
     $movies = [
         $movie1,
@@ -45,7 +66,7 @@
 
             echo "
             <li>
-                ". $movie->titolo . ", " . $movie->durata . ", " . $movie->anno_di_uscita . "
+                ". $movie->titolo . ", " . $movie->durata . ", " . $movie->anno_di_uscita . ", " . $movie->vietato_ai_minori?->setProhibited() . "
             </li>";
 
         }
